8d78f6c7e615b3f9df27ef7d26763b0cf1b892e7
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-13 20:30:24 -0700

    llm added

--- app/llm.py
+++ app/llm.py
@@ -1,1 +1,16 @@
-from 
+from langchain_huggingface import HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="HuggingFaceH4/zephyr-7b-beta",
+    task="conversational",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+    temperature=0.7,
+    max_new_tokens=256
+)
+
+response = llm.invoke("Hello, how are you?")
+print(response)
